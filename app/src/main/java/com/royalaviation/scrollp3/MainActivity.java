8d4fb00dceb09547d48390584665ae843bf721ac
Rohit Priyadarshi <56641101+Royalaviation18@users.noreply.github.com>
2022-07-28 15:17:51 +0530

    Splash Screen Updated

--- app/src/main/java/com/royalaviation/scrollp3/MainActivity.java
+++ app/src/main/java/com/royalaviation/scrollp3/MainActivity.java
@@ -3,6 +3,8 @@ package com.royalaviation.scrollp3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+        Log.d("lifecycle","onCreate invoked");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("lifecycle","onStart invoked");
+        Toast.makeText(this, "Onstart Invoked", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("lifecycle","onResume invoked");
+        Toast.makeText(MainActivity.this,"OnResume Invoked",Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("lifecycle","onPause invoked");
+        Toast.makeText(MainActivity.this, "OnPause Invoked", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("lifecycle","onStop invoked");
+        Toast.makeText(this, "Onstop Invoked", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("lifecycle","onRestart invoked");
+        Toast.makeText(this, "OnRestart Invoked", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("lifecycle","onDestroy invoked");
+        Toast.makeText(this, "OnDestroy Invoked", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/royalaviation/scrollp3/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.royalaviation.scrollp3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                               Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
+
+    }
+}
