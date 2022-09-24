d02f2b423ceda2d9b8f24646906698e0ceb3d5f8
Rohit Priyadarshi <56641101+Royalaviation18@users.noreply.github.com>
2022-09-24 20:11:06 +0530

    Notifications and Shared Preferences Implemented

---
+++ app/src/main/java/com/royalaviation/scrollp3/Notify.java
@@ -0,0 +1,83 @@
+package com.royalaviation.scrollp3;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.util.Date;
+
+public class Notify extends AppCompatActivity {
+
+    Button btnNotify;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notify);
+        getSupportActionBar().hide();
+
+        btnNotify = findViewById(R.id.btnNotify);
+
+        btnNotify.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MovieRec.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+//                File root = Environment.getExternalStorageDirectory();
+//                Bitmap bMap = BitmapFactory.decodeFile(root + "drawable/ic_baseline_notifications_24.xml");
+
+                createNotificationChannel();
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "CHANNEL_ID")
+                        .setSmallIcon(R.drawable.ic_baseline_notifications_active_24)
+                        .setContentTitle("Hey, Come Back! \n")
+                        .setContentText("Checkout new Movies")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+//                        .setColor(R.drawable.bmbackgroundtwo)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.alexanot))
+                        //timestamp
+                        .setWhen((new Date()).getTime())
+                        .setShowWhen(true)
+                        .setColorized(true)
+                        // Set the intent that will fire when the user taps the notification
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true);
+
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+
+
+// notificationId is a unique int for each notification that you must define
+                notificationManager.notify(1, builder.build());
+            }
+        });
+
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void createNotificationChannel() {
+
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel("CHANNEL_ID", "Pankaj", importance);
+        channel.setDescription("Click");
+
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+    }
+}
--- app/src/main/java/com/royalaviation/scrollp3/SettingsFragment.java
+++ app/src/main/java/com/royalaviation/scrollp3/SettingsFragment.java
@@ -1,7 +1,13 @@
 package com.royalaviation.scrollp3;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,12 +18,18 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Objects;
 
 public class SettingsFragment extends Fragment {
     @Nullable
@@ -34,14 +46,20 @@ public class SettingsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
         lvView = getActivity().findViewById(R.id.lvItems);
-        fab  = getActivity().findViewById(R.id.fbAlexa);
+        fab = getActivity().findViewById(R.id.fbAlexa);
         fab.show();
+
         ArrayList<String> arrayList = new ArrayList<>();
         arrayList.add("Profile");
         arrayList.add("Logout");
         arrayList.add("Rate Us");
+        arrayList.add("Notify");
+
+
         ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, arrayList);
         lvView.setAdapter(arrayAdapter);
+
+
 
         lvView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
@@ -50,8 +68,13 @@ public class SettingsFragment extends Fragment {
                 if (i == 2) {
                     startActivity(new Intent(getActivity(), RateUs.class));
                 }
+                if(i == 3){
+                    startActivity(new Intent(getActivity(),Notify.class));
+                }
             }
         });
 
     }
+
+
 }
